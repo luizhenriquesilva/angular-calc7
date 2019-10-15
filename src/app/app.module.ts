4ba8e755f97b893aa6fd10fcf66e153656d7eb48
luizhenriquesilva <luiz.h85@hotmail.com>
2019-10-15 20:04:24 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { LogComponent } from './log.component";
+import { AppComponent } from "./app.component";
+import { LogComponent } from "./log.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, LogComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, LogComponent],
+  bootstrap: [AppComponent, LogComponent]
 })
-export class AppModule { }
+export class AppModule {}
